7c9c05420b9d7eef83496616ceab5b2ff1843265
sinemaltinkilit <sinemaltinkilit@gmail.com>
2016-04-18 15:59:38 +0300

    rehber app

---
+++ rehberApp/Models/rehber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace rehberApp.Models
+{
+    public class rehber
+    {
+        [Key]
+        public int id { get; set; }
+        public string adi { get; set; }
+        public string soyadi { get; set; }
+        public int numara { get; set; }
+
+    }
+}
